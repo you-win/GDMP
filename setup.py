ac3f65a9eaf04833b2e42597f96ab2a9f4de630d
Tim Yuen <timothy.yuen22@gmail.com>
2022-10-01 14:10:29 -0400

    add github workflow for linux

--- setup.py
+++ setup.py
@@ -24,19 +24,13 @@ godot_build = os.path.join(os.path.dirname(__file__), 'godot.BUILD')
 third_party_dir = os.path.join(mediapipe_dir, 'third_party')
 copy(godot_build, third_party_dir)
 
-# Symlink or copy GDMP source code to mediapipe workspace
-try:
-    copy_dir = os.path.join(os.path.dirname(__file__), 'GDMP')
-    dest_dir = os.path.join(mediapipe_dir, 'mediapipe', 'GDMP')
-    if not os.path.isdir(dest_dir):
-        os.symlink(copy_dir, dest_dir, True)
-except OSError:
-    print("Symlink not available, procceed to copy files.")
-    if os.path.isdir(dest_dir):
-        rmtree(dest_dir)
-    copytree(copy_dir, dest_dir)
+copy_dir = os.path.join(os.path.dirname(__file__), 'GDMP')
+dest_dir = os.path.join(mediapipe_dir, 'mediapipe', 'GDMP')
+
+copytree(copy_dir, dest_dir)
 
 # Symlink external dependencies
+print("Trying to symlink external dependencies to mediapipe")
 try:
     external_dir = os.path.join(mediapipe_dir, "external")
     os.symlink("bazel-out/../../../external", external_dir, True)
